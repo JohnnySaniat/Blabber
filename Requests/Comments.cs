27cd65921cebd457cfaee7c13bd48ff7c89f46f5
Alex Sendre <rares.sendre@gmail.com>
2024-03-16 11:34:37 -0500

    add crud requests for comments and crd for reactions

--- Requests/Comments.cs
+++ Requests/Comments.cs
@@ -1,10 +1,101 @@
-﻿namespace Blabber.Requests
+﻿using Microsoft.EntityFrameworkCore;
+using Blabber.Models;
+using System.Data.Common;
+using Blabber;
+
+namespace E24_Amazon.Requests
 {
     public class Comments
     {
         public static void Map(WebApplication app)
         {
-            // requests go inside here
+            // VIEW POST COMMENTS
+            app.MapGet("/posts/{postId}/comments", (BlabberDbContext db, int postId) =>
+            {
+                var post = db.Posts.Include(p => p.Comments).Include(p => p.User).SingleOrDefault(p => p.Id == postId);
+
+                if (post == null)
+                {
+                    return Results.NotFound("Unable to find requested data");
+                }
+
+                var response = new
+                {
+                    id = post.Id,
+                    title = post.Title,
+                    image = post.Image,
+                    comments = post.Comments.Select(comment => new
+                    {
+                        id = comment.Id,
+                        authorId = comment.AuthorId,
+                        postId = comment.PostId,
+                        content = comment.Content,
+                        createdOn = comment.CreatedOn,
+                    }),
+                };
+
+                return Results.Ok(response);
+            });
+
+            // ADD A COMMENT TO A POST
+            app.MapPost("/posts/{postId}/comments", (BlabberDbContext db, string comment, int postId, int authorId) =>
+            {
+                if (comment == null)
+                {
+                    return Results.NotFound("You must type something to submit");
+                }
+
+                var response = new Comment
+                {
+                    PostId = postId,
+                    AuthorId = authorId,
+                    Content = comment,
+                };
+
+                try
+                {
+                    db.Comments.Add(response);
+                    db.SaveChanges();
+                    return Results.Created($"/posts/{postId}/comments/{response.Id}", response);
+                }
+                catch (DbException ex)
+                {
+                    return Results.BadRequest($"There was an error: {ex.Message}");
+                }
+            });
+
+            // UPDATE EXISTING COMMENT
+            app.MapPut("/posts/{postId}/comments/{commentId}", (BlabberDbContext db, int postId, int commentId, string updatedComment) =>
+            {
+                var post = db.Posts.Include(p => p.Comments).SingleOrDefault(p => p.Id == postId);
+                Comment commentToUpdate = db.Comments.SingleOrDefault(c => c.Id == commentId);
+
+                if (commentToUpdate == null)
+                {
+                    return Results.NotFound("There was an error, the comment you requested was not found");
+                }
+
+                commentToUpdate.Content = updatedComment;
+
+                db.SaveChanges();
+                return Results.NoContent();
+            });
+
+            // DELETE SPECIFIC COMMENT FROM A SPECIFIC POST
+            app.MapDelete("/posts/{postId}/comments/{commentId}", (BlabberDbContext db, int postId, int commentId) =>
+            {
+                var post = db.Posts.Include(p => p.Comments).FirstOrDefault(p => p.Id == postId);
+                var comment = db.Comments.Find(commentId);
+
+                if (post == null || comment == null)
+                {
+                    return Results.NotFound("Unable to find the requested data");
+                }
+
+                post.Comments.Remove(comment);
+                db.SaveChanges();
+                return Results.NoContent();
+            });
         }
     }
 }
--- Requests/Reactions.cs
+++ Requests/Reactions.cs
@@ -1,10 +1,142 @@
-﻿namespace Blabber.Requests
+﻿using Blabber;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using Blabber.DTOs;
+using Blabber.Models;
+
+namespace E24_Amazon.Requests
 {
     public class Reactions
     {
         public static void Map(WebApplication app)
         {
-            // requests go inside here
+            // VIEW ALL REACTIONS
+            app.MapGet("/reactions", (BlabberDbContext db) =>
+            {
+                return db.Reactions.ToList();
+            });
+
+            // VIEW REACTION DETAILS 
+            app.MapGet("/reactions/{id}", (BlabberDbContext db, int id) =>
+            {
+                var reaction = db.Reactions.Include(r => r.Posts).SingleOrDefault(r => r.Id == id);
+
+                if (reaction == null)
+                {
+                    return Results.NotFound("Unable to find requested data");
+                }
+
+                var response = new
+                {
+                    id = reaction.Id,
+                    label = reaction.Label,
+                    image = reaction.Image,
+                    posts = reaction.Posts.Select(post => new
+                    {
+                        id = post.Id,
+                        title = post.Title,
+                        image = post.Image,
+                        content = post.Content
+                    }),
+                };
+
+                return Results.Ok(response);
+            });
+
+            // VIEW POST REACTIONS
+            app.MapGet("/posts/{postId}/reactions", (BlabberDbContext db, int postId) =>
+            {
+                var post = db.Posts.Include(p => p.Reactions).SingleOrDefault(p => p.Id == postId);
+
+                if (post == null)
+                {
+                    return Results.NotFound("Unable to find post");
+                }
+
+                var response = new
+                {
+                    id = post.Id,
+                    title = post.Title,
+                    image = post.Image,
+                    reactions = post.Reactions.Select(reaction => new
+                    {
+                        id = reaction.Id,
+                        label = reaction.Label,
+                        image = reaction.Image
+                    })
+                };
+
+                return Results.Ok(response);
+            });
+
+            // CREATE A REACTION
+            app.MapPost("/reactions", (BlabberDbContext db, Reaction newReaction) =>
+            {
+                try
+                {
+                    db.Reactions.Add(newReaction);
+                    db.SaveChanges();
+                    return Results.Created($"/reactions/{newReaction.Id}", newReaction);
+                }
+                catch (DbException)
+                {
+                    return Results.NotFound("Invalid data submitted.");
+                }
+            });
+
+            // DELETE A REACTION
+            app.MapDelete("/reactions/{id}", (BlabberDbContext db, int id) =>
+            {
+                var reaction = db.Reactions.SingleOrDefault(r => r.Id == id);
+
+                if (reaction == null)
+                {
+                    return Results.NotFound();
+                }
+
+                db.Reactions.Remove(reaction);
+                db.SaveChanges();
+                return Results.NoContent();
+            });
+
+            // ADD A REACTION TO POST
+            app.MapPost("/posts/{postId}/reactions", (BlabberDbContext db, PostReactionsDTO newReaction) =>
+            {
+                var post = db.Posts.Include(p => p.Reactions).FirstOrDefault(p => p.Id == newReaction.PostId);
+                var reactionToAdd = db.Reactions.Find(newReaction.ReactionId);
+
+                if (post == null || reactionToAdd == null)
+                {
+                    return Results.NotFound();
+                }
+
+                try
+                {
+                    post.Reactions.Add(reactionToAdd);
+                    db.SaveChanges();
+                    return Results.Created($"/posts/{newReaction.PostId}/reactions/{newReaction.ReactionId}", reactionToAdd);
+                }
+                catch
+                {
+                    return Results.BadRequest("There was an error with the data submitted");
+                }
+            });
+
+            // REMOVE A REACTION FROM A POST
+            app.MapDelete("/posts/{postId}/reactions/{reactionId}", (BlabberDbContext db, int postId, int reactionId) =>
+            {
+                var post = db.Posts.Include(p => p.Reactions).FirstOrDefault(p => p.Id == postId);
+                var reaction = db.Reactions.Find(reactionId);
+
+                if (post == null || reaction == null)
+                {
+                    return Results.NotFound("Unable to find the requested data");
+                }
+
+                post.Reactions.Remove(reaction);
+                db.SaveChanges();
+                return Results.NoContent();
+            });
         }
     }
 }
